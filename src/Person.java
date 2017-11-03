b66b2e1ccb90285c55053f83d23e05fe3638b86e
albardespinozaaldaco <albardespinoza@gmail.com>
2017-11-02 21:45:03 -0700

    Added comment.

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,4 @@
-public class Person {
+public class Person {//Person variable
     //Instance variables
     private String firstName;//Anything private can only be used in the same class the private variable is.
     private String lastName;
