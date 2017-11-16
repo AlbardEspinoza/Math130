58e64df42dc2ff7db66f622517544c22493f875a
albardespinozaaldaco <albardespinoza@gmail.com>
2017-11-15 22:46:48 -0800

    Improved the code and added comments.

--- src/HWSix.java
+++ src/HWSix.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-
 public class HWSix {
     public static void main(String[] args) {
         int[] a = {90, 34, 22};
         Student b = new Student("Josh", "Andrews", a);
-        int[] c = {95, 67, 49};
+        int[] c = {95, 70, 49};
         Student d = new Student("Jorge", "Gonzales", c);
-        System.out.println("Name: " + b.getFirstName() + " " + b.getLastName() + "\nHighest score: " + Arrays.toString(b.getGrades()) + "\nLowest score: " + b.getLowestGrade());
+        System.out.println("Name: " + b.getFirstName() + " " + b.getLastName() + "\nHighest Score: " + b.getHighestGrade() + "\nLowest Score: " + b.getLowestGrade() + "\nPassing Scores: " + b.getNumberPassing());
+        System.out.println("\nName: " + d.getFirstName() + " " + d.getLastName() + "\nHighest Score: " + d.getHighestGrade() + "\nLowest Score: " + d.getLowestGrade() + "\nPassing Scores: " + d.getNumberPassing());
+        b.setFirstName("Carmen");
+        b.setLastName("Garcia");
+        d.setFirstName("David");
+        d.setLastName("Williams");
+        System.out.println("Name: " + b.getFirstName() + " " + b.getLastName() + "\nHighest Score: " + b.getHighestGrade() + "\nLowest Score: " + b.getLowestGrade() + "\nPassing Scores: " + b.getNumberPassing());
+        System.out.println("\nName: " + d.getFirstName() + " " + d.getLastName() + "\nHighest Score: " + d.getHighestGrade() + "\nLowest Score: " + d.getLowestGrade() + "\nPassing Scores: " + d.getNumberPassing());
     }
 }
